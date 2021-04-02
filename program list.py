ee40f54d684ab39b17ec198879449ada2857c791
Ansari Arif <62074488+arif123-stack@users.noreply.github.com>
2021-04-02 23:01:50 +0530

    new commit

---
+++ program list.py
@@ -0,0 +1,340 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Basic Programs list:
+
+
+# In[2]:
+
+
+# 1.Program to calculate the area of rectangle
+
+
+# In[ ]:
+
+
+width=input("Enter width")
+height=input("Enter Height")
+area = width*height
+print("Area of rectangle : "+str( area))
+
+
+# In[ ]:
+
+
+# 2.Program to calculate the volume of sphere
+
+
+# In[ ]:
+
+
+*radius(=input("enter, radius"))
+volume=(4.0/3.0)*3.14*(radius*radius*radius)
+print("Volume of the sphere = "+str(volume))
+
+
+# In[ ]:
+
+
+# 3.Program to find the area of a pentagon
+
+
+# In[ ]:
+
+
+s=input("enter the side")
+print("s is :"+s)
+a=input("enter the apothem length")
+print("a is :"+a)
+Area=(5.0/2.0)*s*a
+print("Enter the pentagon : "+str(Area))
+
+
+# In[ ]:
+
+
+# 4.Program to find the area of the parallelogram
+
+
+# In[ ]:
+
+
+base=4  
+height=18  
+area_parallelogram=base*height  
+print("Area of the parallelogram="+str(area_parallelogram))
+
+
+# In[ ]:
+
+
+# 5. Program to find the area of square
+
+
+# In[ ]:
+
+
+s=13  
+area_square=s*s  
+print("Area of the square="+str(area_square)) 
+
+
+# In[ ]:
+
+
+# 6.Program to find the surface area of sphere
+
+
+# In[ ]:
+
+
+radius=37  
+pie=3.14285714286  
+area_sphere=4*pie*(radius*radius)  
+print("Surface area of the sphere="+str(area_sphere))
+
+
+# In[ ]:
+
+
+# 7. Program to find the volume of cone
+
+
+# In[ ]:
+
+
+height=38  
+radius=35  
+pie=3.14285714286  
+volume=pie*(radius*radius)*height/3  
+print("volume of the cone="+str(volume))  
+
+
+# In[ ]:
+
+
+# 8.Program to find the volume of the cube
+
+
+# In[ ]:
+
+
+a=4  
+volume=a*a*a  
+print("volume of the cube="+str(volume))  
+
+
+# In[ ]:
+
+
+# 9. Program to find the volume of cylinder
+
+
+# In[ ]:
+
+
+height=38  
+radius=35  
+pie=3.14285714286  
+volume=pie*(radius*radius)*height  
+print("volume of the cube="+str(volume))  
+
+
+# In[ ]:
+
+
+# 10.Program to calculate the CGPA percentage
+
+
+# In[ ]:
+
+
+English = 9.1  
+Hindi = 8.5  
+Maths = 9.5  
+Science =9.6;  
+SocialStudy = 8.6  
+CGPA = (9.1+8.5+9.5+9.6+8.6)/(5.0)  
+CGPAper = 9.5 * (CGPA)  
+print(" CGPA percentage is:");  
+print(CGPAper);   
+
+
+# In[ ]:
+
+
+# 11.Program to convert Celsius into Fahrenheit
+
+
+# In[ ]:
+
+
+celsius= 12  
+fahrenheit= ((celsius*9)/5)+32  
+print("Temperature in Fahrenheit is: ")
+print(fahrenheit)  
+
+
+# In[ ]:
+
+
+# 12.Program to convert days into years
+
+
+# In[ ]:
+
+
+days= 5555  
+years= days / 365  
+print("Number of years is: ") 
+print(years)  */
+
+
+# In[ ]:
+
+
+# 13.program to convert Fahrenheit into Celsius
+
+
+# In[ ]:
+
+
+Fahrenheit= 54  
+Celsius = ((Fahrenheit-32)*5)/9  
+print("Temperature in Celsius is: ")  
+print(Celsius)
+
+
+# In[ ]:
+
+
+# 14.Program to find the area of an equilateral triangle
+
+
+# In[ ]:
+
+
+a= 5  
+area = ( 1.73 * a*a) / 4  
+print("Area of Equilateral Triangle is: ")  
+print(area) 
+
+
+# In[ ]:
+
+
+# 15.Program to find the area of a triangle
+
+
+# In[ ]:
+
+
+b= 5  
+h=12  
+area = (b*h)/2  
+print("Area of Triangle is :")  
+print(area)  
+
+
+# In[ ]:
+
+
+# 16.Program to find the area of the right angle triangle
+
+
+# In[ ]:
+
+
+b = 5  
+h = 8  
+area = ( b * h) / 2  
+print("Area of Right Angle Triangle is :")  
+print(area)
+
+
+# In[ ]:
+
+
+#17.Program to find the perimeter of the rectangle
+
+
+# In[ ]:
+
+
+a=c=2 # in rectangle sides opposite to each other are equal in length  
+b=d=4 # length of opposite sides  
+Perimeter = 2*(a+ b)  
+print("Perimeter of rectangle is: ")  
+print(Perimeter);   
+
+
+# In[ ]:
+
+
+# 18.Program to find the simple interest
+
+
+# In[ ]:
+
+
+P= 5000 #Principal Amount  
+R=15 #Rate   
+T=1 #Time  
+SI  = (P*R*T)/100; # Simple Interest calculation  
+print("Simple Interest is :") 
+print(SI);  #prints Simple Interest  
+
+
+# In[ ]:
+
+
+# 19.Program to find the surface area of a cube
+
+
+# In[ ]:
+
+
+l =5  
+b = 5  
+h = 5  
+a=l=b=h  
+surfacearea =6*(a*a)  
+print("Surface Area of Cube is : ")  
+print (surfacearea)
+
+
+# In[ ]:
+
+
+# 20.Program to find the surface area of cuboid
+
+
+# In[ ]:
+
+
+l = 2  
+w = 3  
+h = 5  
+surfacearea =2*( l *w  + w* h + h*l)  
+print("Surface Area of Cuboid is : ");  
+print (surfacearea);  
+
+
+# In[ ]:
+
+
+# 21.Program to find the surface area of the cylinder
+
+
+# In[ ]:
+
+
+r = 2.0  #radius  
+h = 5.0 #height  
+surfacearea = (2*22*(r + h))/7 #calculation  
+print ("Surface Area Of Cylinder : ");  
+print (surfacearea); #prints surface area  
+
